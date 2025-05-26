b825d7b2c6c473eaf7227aa4e4f6add1843bd810
Craftbuilder <craftbuilder119@gmail.com>
2025-05-26 07:12:04 +0600

    fixed errors

--- auth/firebase_auth.py
+++ auth/firebase_auth.py
@@ -2,7 +2,17 @@ import os
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
+# Get the current script directory
+current_dir = os.path.dirname(__file__)
+
+# Go to the parent directory (project/)
+project_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+# Build full path to data.txt
+file_path = os.path.join(project_dir, 'serviceAccountKey.json')
+
+# service_account_path = r'C:\Users\user\OneDrive\Desktop\Tonmoy\Projects\Client 08 [VPN-App Integration]\vpn-backend\serviceAccountKey.json'
+cred = credentials.Certificate(file_path)
 firebase_admin.initialize_app(cred)
 
 def verify_token(id_token):
